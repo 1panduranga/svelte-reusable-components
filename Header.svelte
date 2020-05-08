20f03e3b79456d97292ab5f91666f9ba00f39712
pandu ranga <54597214+1panduranga@users.noreply.github.com>
2020-05-08 13:54:59 +0530

    basic header component
    takes slot and displays  title at the center

---
+++ Header.svelte
@@ -0,0 +1,34 @@
+<header>
+<div>
+ <h4>
+     <slot></slot>
+ </h4>
+</div>
+
+</header>
+   
+
+<style>
+div{
+    text-align: center;
+    background-color: rgb(255,230,194);
+    border-radius: 6px;
+    margin:0px auto;
+}
+header{
+    text-align: center;
+    
+}
+h4{
+    padding:30px;
+    margin:0px;
+    color:brown;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-weight: normal;
+}
+@media (max-width: 640px) {
+		h4{
+            padding:10px;
+        }
+	}
+</style>
