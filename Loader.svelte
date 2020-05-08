32091be54e971ee9bc5e44bf621c272cb6b37a0a
pandu ranga <54597214+1panduranga@users.noreply.github.com>
2020-05-08 13:51:00 +0530

    loader component
    takes color,size and width as optional props

---
+++ Loader.svelte
@@ -0,0 +1,34 @@
+<script>
+export let color="tomato";
+export let width="5";
+export let size=50;
+</script>
+<div class="wrapper">
+<div class="loader" style='border-top: {width}px solid {color};border-width:{width}px;width:{size}px;
+  height:{size}px;'></div>
+</div>
+
+
+<style>
+
+.loader {
+    text-align: center;
+    margin:10px auto;
+  border:solid #f3f3f3;
+  border-radius: 50%;
+  
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
